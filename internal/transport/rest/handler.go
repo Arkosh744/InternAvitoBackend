23c8d5aa31f5d8844f47ef4f459d70be8f80d02c
Arkosh <trukan24@gmail.com>
2022-11-03 15:04:51 +0300

    Clean server with Echo Init

--- internal/transport/rest/handler.go
+++ internal/transport/rest/handler.go
@@ -1,1 +1,21 @@
 package rest
+
+import "github.com/labstack/echo/v4"
+
+type Users interface {
+}
+
+type Handler struct {
+	usersService Users
+}
+
+func NewHandler(users Users) *Handler {
+	return &Handler{
+		usersService: users,
+	}
+}
+
+func (h *Handler) InitRouter() *echo.Echo {
+	router := echo.New()
+	return router
+}
--- pkg/database/psql.go
+++ pkg/database/psql.go
@@ -1,1 +1,33 @@
 package database
+
+import (
+	"database/sql"
+	"fmt"
+	log "github.com/sirupsen/logrus"
+)
+
+type ConnectionInfo struct {
+	Host     string
+	Port     int
+	Username string
+	DBName   string
+	SSLMode  string
+	Password string
+}
+
+func NewPostgresConnection(info ConnectionInfo) (*sql.DB, error) {
+	connectString := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=%s password=%s",
+		info.Host, info.Port, info.Username, info.DBName, info.SSLMode, info.Password)
+	log.Println(connectString)
+	db, err := sql.Open("postgres", connectString)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
